4cb0a1827257909488e6e1a3bdac8a79e5832886
Roberto Obando <robertoandres24@gmail.com>
2020-01-08 17:04:57 -0300

    create browser-api.js

---
+++ browser-api.js
@@ -0,0 +1,204 @@
+/////////////////
+//             //
+// CHALLENGE 1 //
+//             //
+/////////////////
+
+ /* <<<=== Remove the first two slashes (//) to comment out this challenge when finished
+console.log('Start of Challenge 1');
+// ...your code below
+console.log('beginning of code')
+setTimeout(function() { console.log('im in the settimeout'); }, 0)
+console.log('end of the code')
+
+
+
+
+
+console.log('End of Challenge 1');
+// */// (do not alter this line)
+
+
+
+
+/////////////////
+//             //
+// CHALLENGE 2 //
+//             //
+/////////////////
+
+ /* <<<=== Remove the first two slashes (//) to comment out this challenge when finished
+console.log('Start of Challenge 2');
+// ...your code below
+let intervalId = setInterval(function(){
+  console.log('interval hello')
+},2000)
+
+setTimeout(function() {
+  clearInterval(intervalId)
+}, 10000)
+
+
+
+
+// ...your code above
+function clearAllIntervals() {
+  for (let i = 0; i < 1000; i++) {
+    clearInterval(i);
+  }
+}
+console.log('End of Challenge 2');
+// */// (do not alter this line)
+
+
+
+/////////////////
+//             //
+// CHALLENGE 3 //
+//             //
+/////////////////
+
+/* <<<=== Remove the first two slashes (//) to comment out this challenge when finished
+console.log('Start of Challenge 3');
+// ...your code below
+function sayHowdy() {
+  console.log('Howdy')
+}
+function everyXsecsForYsecs(func, interval, total) {
+  let intervalId = setInterval(func,interval)
+
+  setTimeout(function() {
+    clearInterval(intervalId)
+  }, total)
+}
+
+everyXsecsForYsecs(sayHowdy, 1000, 5000)
+
+
+console.log('End of Challenge 3');
+// */// (do not alter this line)
+
+
+
+
+/////////////////
+//             //
+// CHALLENGE 4 //
+//             //
+/////////////////
+
+// /* <<<=== Remove the first two slashes (//) to comment out this challenge when finished
+console.log('Start of Challenge 4');
+// ...your code below
+
+
+
+
+
+console.log('End of Challenge 4');
+// */// (do not alter this line)
+
+
+
+/////////////////
+//             //
+// CHALLENGE 5 //
+//             //
+/////////////////
+
+// /* <<<=== Remove the first two slashes (//) to comment out this challenge when finished
+console.log('Start of Challenge 5');
+// ...your code below
+
+
+
+
+// ...your code above
+document.body.style.background = '#def';
+console.log('End of Challenge 3');
+// */// (do not alter this line)
+
+
+
+/////////////////
+//             //
+// CHALLENGE 6 //
+//             //
+/////////////////
+
+// /* <<<=== Remove the first two slashes (//) to comment out this challenge when finished
+console.log('Start of Challenge 6');
+var dataReceived;
+
+function ajaxSimulate(id, callback) {
+  var database = ['Aaron', 'Barbara', 'Chris'];
+
+}
+// ...your code below
+
+
+
+console.log('End of Challenge 6');
+// */// (do not alter this line)
+
+
+
+/////////////////
+//             //
+// CHALLENGE 7 //
+//             //
+/////////////////
+
+// /* <<<=== Remove the first two slashes (//) to comment out this challenge when finished
+console.log('Start of Challenge 7');
+// ...your code below
+
+
+
+
+
+console.log('End of Challenge 7');
+// */// (do not alter this line)
+
+
+
+/////////////////
+//             //
+// CHALLENGE 8 //
+//             //
+/////////////////
+
+// /* <<<=== Remove the first two slashes (//) to comment out this challenge when finished
+console.log('Start of Challenge 8');
+// ...your code below
+
+
+
+
+
+
+console.log('End of Challenge 8');
+// */// (do not alter this line)
+
+
+
+/////////////////
+//             //
+// CHALLENGE 9 //
+//             //
+/////////////////
+
+// /* <<<=== Remove the first two slashes (//) to comment out this challenge when finished
+console.log('Start of Challenge 9');
+// ...your code below
+
+
+
+
+
+
+
+console.log('End of Challenge 9');
+// */// (do not alter this line)
+
+
